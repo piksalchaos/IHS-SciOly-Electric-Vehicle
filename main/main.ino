3ddea6da65fc881ed5b3d9f17958318abaa8e8b6
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-03-03 03:54:05 -0800

    finally got motor to move with arduino

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+const int ESC_PIN = 9;
+
+const int MIN_PWM = 1000;
+const int MAX_PWM =  2000;
+
+Servo esc;
+bool run_started = false;
+
+void begin_esc_calibration() {
+  Serial.println("Starting ESC calibration...");
+  
+  Serial.println("Full throttle - Power on ESC now!");
+  esc.writeMicroseconds(MAX_PWM);
+  delay(3000);
+  
+  Serial.println("Zero throttle - Calibration should complete.");
+  esc.writeMicroseconds(MIN_PWM);
+  delay(3000);
+
+  Serial.println("Calibration complete!");
+}
+void setup() {
+  esc.attach(ESC_PIN, MIN_PWM, MAX_PWM);
+  Serial.begin(9600);
+  
+  begin_esc_calibration();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // little program that tests the max speed, half speed, and no speed
+  Serial.println("Begin loop");
+  delay(3000);
+  esc.write(180);
+  delay(3000);
+  esc.write(90);
+  delay(3000);
+  esc.write(0);
 }
